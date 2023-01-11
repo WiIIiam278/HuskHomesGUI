e60f9422a6ce1f25d994d031b6e9e30f12011c76
From-pErfo <Nm70727@qq.com>
2023-01-11 14:43:53 +0800

    test

--- src/main/java/net/william278/huskhomes/gui/SavedPositionMenu.java
+++ src/main/java/net/william278/huskhomes/gui/SavedPositionMenu.java
@@ -205,6 +205,10 @@ public class SavedPositionMenu {
         // p = 开放 (phome)
         // r = 删除
 
+        this.edit_menu.setCloseAction(i -> {
+            return false;
+        });
+
         // 背景
         this.edit_menu.addElement(new StaticGuiElement('a',
                 new ItemStack(switch (menuType) {
